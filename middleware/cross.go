847647f145ef7b4ac0a78b3612421026b62017aa
jianghong <1637124239@qq.com>
2026-01-06 10:49:59 +0800

    x_access

--- middleware/cross.go
+++ middleware/cross.go
@@ -7,7 +7,7 @@ func CrossSite(c *gin.Context) {
 	//服务器支持的所有跨域请求的方法
 	c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE,UPDATE")
 	//允许跨域设置可以返回其他子段，可以自定义字段
-	c.Header("Access-Control-Allow-Headers", "Authorization, Content-Length, X-CSRF-Token, Token,session")
+	c.Header("Access-Control-Allow-Headers", "Authorization, Content-Length, X-CSRF-Token, Token,session,x_access")
 	// 允许浏览器（客户端）可以解析的头部 （重要）
 	c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers")
 	//允许客户端传递校验信息比如 cookie (重要)
